8ee2fbbad4d94b6d9d07b3ad564b1312cf03dddf
Wenya Rowe <roweww@lmem21.be-md.ncbi.nlm.nih.gov>
2019-10-17 23:21:35 -0400

    clean the copy by remove prints

--- cluster.py
+++ cluster.py
@@ -2,19 +2,21 @@ import pandas as pd
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 import collections
-
+import copy
 
 quiry_df = pd.read_excel("SiteSearch2019-01-uniquesForStep2.xlsx")
 n_freq = 200
-n_bucket = 3
+n_bucket = 10
 pair = []
 whole_list = []
-bucket = [[],[], []]#[[]]*n_bucket
+bucket_init = [[]  for i in range(n_bucket)]
+bucket = bucket_init
+#bucket = [[],[], [], [], []]#[[]]*n_bucket
 print("bucket = ", bucket)
 
-for i_comp1 in range(500):
+for i_comp1 in range(2000):
     comp1 = quiry_df['adjustedQueryTerm'][i_comp1]
-    for i_comp2 in range(i_comp1+1, 500):
+    for i_comp2 in range(i_comp1+1, 2000):
         comp2 = quiry_df['adjustedQueryTerm'][i_comp2]
         score = fuzz.ratio(comp1, comp2)
         if (score > 75):
@@ -29,18 +31,12 @@ i_cur = 0
 i = 0
 range_check = 0
 for key, value in (whole_key):
-#    print("key = ", key, "value = ", value)
     key_in_pre = False
 # check whether key in previous bucket
     for j_check in range(max(range_check, i_end)):
-#        print("j_check = ", j_check)
-#        print("key = ", key)
-#        print("bucket[j_check] = ", bucket[j_check])
-#        print("key in buc ", key in bucket[j_check])
         if key in bucket[j_check]:
             key_in_pre = key in bucket[j_check]
             i_cur = j_check
-#    print( "What is i ", i)
     if (i_end == 0 and (bucket[0] == [])):
         i = 0
         range_check = 1
@@ -59,19 +55,14 @@ for key, value in (whole_key):
     pair_copy = pair.copy()
     if (i < n_bucket):
       for i_pair in pair_copy:
-#        print("i_pair = ", i_pair)
         if(key == i_pair[0]):
             bucket[i].extend(i_pair)
             index = pair.index((key, i_pair[1]))
-#            print("index = ", index)
             pair.pop(index)            
         elif(key == i_pair[1]):          
             bucket[i].extend(i_pair)
-#        print( "bucket = ", bucket)    
 
 
-
-#print(bucket)
+print( "set ", set(bucket[0]), set(bucket[1]), set(bucket[2]))
 for ii in range(n_bucket):
-    print( "set ", set(bucket[0]), set(bucket[1]), set(bucket[2]))
     print("bucket ", ii, " = ", [quiry_df['adjustedQueryTerm'][i_name] for i_name in set(bucket[ii])])
